f419bfd1513e8d88c9e55b899b917af4310b8b98
guido <guido.molino@across.it>
2021-05-24 13:02:57 +0200

    messages

--- app/Http/Controllers/Api/MessageController.php
+++ app/Http/Controllers/Api/MessageController.php
@@ -3,21 +3,36 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Http\Resources\MessageResource;
-use App\Message;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Http\Resources\MessageResource;
+use App\Http\Requests\StoreMessageRequest;
+use App\Message;
 
 class MessageController extends Controller
 {
-    public function index()
-    {
+    public function index() {
         $users = Message::all();
         return MessageResource::collection($users);
     }
 
-    public function show(Message $message)
-    {
+    public function show(Message $message) {
         return new MessageResource($message);
+    }
+
+    public function store(StoreMessageRequest $request) {
+        $message = Message::create($request->all());
+        return new MessageResource($message);
+    }
+
+    public function update(Message $message, StoreMessageRequest $request) {
+        $message->update($request->all());
+        return new MessageResource($message);
+    }
+
+    public function destroy(message $user){
+        $user->delete();
+        return response(null, Response::HTTP_NO_CONTENT);
     }
 
 }
